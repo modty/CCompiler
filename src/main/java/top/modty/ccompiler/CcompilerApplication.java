8b03691a5db43789870c50454db54ccb1aa4eb01
1997Gongquan <2673077461@qq.com>
2020-05-02 21:10:56 +0800

    2020-05-02 初始化

---
+++ src/main/java/top/modty/ccompiler/CcompilerApplication.java
@@ -0,0 +1,13 @@
+package top.modty.ccompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcompilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcompilerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/modty/ccompiler/CcompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.modty.ccompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcompilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
